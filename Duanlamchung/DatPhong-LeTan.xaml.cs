e6f0c032397783c333911062d90372f3a3ec7be7
MinhHwy <huyuwu55@gmail.com>
2026-02-25 16:53:31 +0700

    add FORM datphong,dashboard,rommap cua letan

---
+++ Duanlamchung/DatPhong-LeTan.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Duanlamchung
+{
+    /// <summary>
+    /// Interaction logic for DatPhong_LeTan.xaml
+    /// </summary>
+    public partial class DatPhong_LeTan : Window
+    {
+        public DatPhong_LeTan()
+        {
+            InitializeComponent();
+        }
+    }
+}
