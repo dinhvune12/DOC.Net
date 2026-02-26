41728702c15b450ee3dfcf201b74d164876e147e
MinhHwy <huyuwu55@gmail.com>
2026-02-26 14:54:57 +0700

    update form letan

---
+++ Duanlamchung/DashboardLeTan.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Duanlamchung
+{
+    /// <summary>
+    /// Interaction logic for DashboardLeTan.xaml
+    /// </summary>
+    public partial class DashboardLeTan : Window
+    {
+        public DashboardLeTan()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDashboard_Click(object sender, RoutedEventArgs e) { }
+
+        private void btnRoomMap_Click(object sender, RoutedEventArgs e) { }
+
+        private void btnBooking_Click(object sender, RoutedEventArgs e) { }
+
+        private void btnCheckInOut_Click(object sender, RoutedEventArgs e) { }
+
+        private void btnLogout_Click(object sender, RoutedEventArgs e) { }
+
+        private void btnOpenRoomMap_Click(object sender, RoutedEventArgs e) { }
+
+        private void btnCreateBooking_Click(object sender, RoutedEventArgs e) { }
+
+        private void btnQuickCheckInOut_Click(object sender, RoutedEventArgs e) { }
+    }
+}
---
+++ Duanlamchung/DatphongLeTan.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Duanlamchung
+{
+    /// <summary>
+    /// Interaction logic for DatphongLeTan.xaml
+    /// </summary>
+    public partial class DatphongLeTan : Window
+    {
+        public DatphongLeTan()
+        {
+            InitializeComponent();
+        }
+        private void btnCreate_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCancelBooking_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnConfirmCheckIn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
