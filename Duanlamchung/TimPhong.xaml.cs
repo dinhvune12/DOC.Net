d621bd029b5dc1b81c7473bb795c24e0c7e480a0
YuThai <yu382005@gmail.com>
2026-02-27 09:00:53 +0700

    hung update

---
+++ Duanlamchung/TimPhong.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Duanlamchung
+{
+    /// <summary>
+    /// Interaction logic for TimPhong.xaml
+    /// </summary>
+    public partial class TimPhong : Window
+    {
+        public TimPhong()
+        {
+            InitializeComponent();
+        }
+        private void btnTimPhong_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
